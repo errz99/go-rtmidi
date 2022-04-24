e9d67b8e467645eb792c3d05aa0283ecd914f7c2
Matt Taylor <mattrtaylor@users.noreply.github.com>
2022-04-23 23:54:09 -0600

    Use the DUMMY driver for the coverage test.

--- lib.cpp
+++ lib.cpp
@@ -1,3 +1,12 @@
+#if defined(GO_COVERAGE_TEST)
+#undef __MACOSX_CORE__
+#undef __LINUX_ALSA__
+#undef __UNIX_JACK__
+#undef __WINDOWS_MM__
+#undef __WEB_MIDI_API__
+#define __RTMIDI_DUMMY__
+#endif
+
 #include "rtmidi/RtMidi.h"
 
 #define RTMIDI_SOURCE_INCLUDED
